08ba29245ffa79b405632ed402f9ba7ad6d4dd15
jian-ferreira <jianferreiradev@gmail.com>
2023-10-26 12:13:22 -0300

    Commintando o primeiro projeto para teste

---
+++ Program.java
@@ -0,0 +1,9 @@
+// Objetivo: Fazer um teste de criação de repositório;
+// Autor: Jian Ferreira;
+// Última modifcação: 26 / 10 / 2023;
+
+public class Program{
+    public static void main(String[] args) {
+        // Método teste...
+    }
+}
